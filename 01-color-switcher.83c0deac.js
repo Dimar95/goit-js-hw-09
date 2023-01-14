35c61d98b6d734871e6462fd67efb29e0c942541
Dimar95 <110562214+Dimar95@users.noreply.github.com>
2023-01-14 20:02:21 +0000

    Deploying to gh-pages from @ Dimar95/goit-js-hw-09@7d0872479464eab146cefe4663f2a6bcc8963a8d 🚀

---
+++ 01-color-switcher.83c0deac.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.start.addEventListener("click",(()=>function(){t.start.setAttribute("disabled","disabled");setInterval((()=>t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3)}())),t.stop.addEventListener("click",(()=>(clearInterval(timerId),void t.start.removeAttribute("disabled","disabled"))));
+//# sourceMappingURL=01-color-switcher.83c0deac.js.map
